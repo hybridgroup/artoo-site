80cc9f3d2d7092dd57451fb28407ab3a1ab40b3c
Luis Felipe Perez <slashcat@gmail.com>
2014-10-06 11:55:36 -0500

    delete lines to asign margin to elements

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -67,11 +67,9 @@ $(function() {
   $(window).bind('scroll', function() {
     if ($(window).scrollTop() > navpos.top) {
       $('#mainsubnav').addClass('subNavfixed');
-      //$('#HowToConnect, #HowToUse, #Commands, #Events, #Circuit, #Compatability,#SpheroColors,#Drivers,#HowToInstall').addClass('marginTopSubnav');
      }
      else {
        $('#mainsubnav').removeClass('subNavfixed');
-       //$('#HowToConnect, #HowToUse, #Commands, #Events, #Circuit, #Compatability,#SpheroColors,#Drivers,#HowToInstall').removeClass('marginTopSubnav');
        $("#mainsubnav a").removeClass("active");
      }
   });
